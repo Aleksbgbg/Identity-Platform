01eae4fbf1ee19edc337d057d6cd680533689b69
Aleksbgbg <kozichka01@abv.bg>
2019-01-04 22:13:32 +0000

    [UserTable] Add component to display a basic table of users.

---
+++ Identity Platform/Components/UserTable.cs
@@ -0,0 +1,22 @@
+﻿namespace Identity.Platform.Components
+{
+    using Identity.Platform.Models;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserTable : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserTable(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
