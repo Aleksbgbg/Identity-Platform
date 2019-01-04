1b3496bf582a75c1e0f15af723b5a7970d9b7cc0
Aleksbgbg <kozichka01@abv.bg>
2019-01-04 19:46:59 +0000

    [Models] Add Location and NavLocation models which represent routes in the app.

---
+++ Identity Platform/Models/Location.cs
@@ -0,0 +1,22 @@
+﻿namespace Identity.Platform.Models
+{
+    public class Location
+    {
+        public Location(string controller, string action, string name)
+        {
+            Controller = controller;
+            Action = action;
+            Name = name;
+        }
+
+        public Location(string controller, string action) : this(controller, action, controller)
+        {
+        }
+
+        public string Controller { get; }
+
+        public string Action { get; }
+
+        public string Name { get; }
+    }
+}
---
+++ Identity Platform/Models/ViewModels/NavLocation.cs
@@ -0,0 +1,15 @@
+﻿namespace Identity.Platform.Models.ViewModels
+{
+    public class NavLocation
+    {
+        public NavLocation(bool isActive, Location location)
+        {
+            IsActive = isActive;
+            Location = location;
+        }
+
+        public bool IsActive { get; }
+
+        public Location Location { get; }
+    }
+}
