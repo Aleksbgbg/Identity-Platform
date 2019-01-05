82feb1d4f69f2e0e537722de09787c567489f921
Aleksbgbg <kozichka01@abv.bg>
2019-01-05 17:06:30 +0000

    [AdminsController] Add AdminsController with Index view.

---
+++ Identity Platform/Controllers/AdminsController.cs
@@ -0,0 +1,14 @@
+﻿namespace Identity.Platform.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = "Admin")]
+    public class AdminsController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
