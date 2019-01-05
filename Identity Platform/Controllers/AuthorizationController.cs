97d320d43323a930ad98d09d9e627d1ec4dab106
Aleksbgbg <kozichka01@abv.bg>
2019-01-05 16:48:40 +0000

    [AuthorizationController] Add controller for authorization, with login functionality

---
+++ Identity Platform/Controllers/AuthorizationController.cs
@@ -0,0 +1,72 @@
+﻿namespace Identity.Platform.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Identity.Platform.Models;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+    [AllowAnonymous]
+    public class AuthorizationController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AuthorizationController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl = "/")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View("SignInForm");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("SignInForm");
+            }
+
+            AppUser targetUser = await _userManager.FindByNameAsync(login.Username);
+
+            if (targetUser == null)
+            {
+                ModelState.AddModelError(nameof(login.Username), "Invalid username");
+                return View("SignInForm");
+            }
+
+            SignInResult signInResult = await _signInManager.PasswordSignInAsync
+            (
+                targetUser,
+                login.Password,
+                isPersistent: false,
+                lockoutOnFailure: false
+            );
+
+            if (signInResult.Succeeded)
+            {
+                return Redirect(returnUrl ?? "/");
+            }
+
+            ModelState.AddModelError(nameof(login.Password), "Invalid password");
+            return View("SignInForm");
+
+        }
+    }
+}
