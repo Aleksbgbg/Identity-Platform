80f80ac366c72ec2ef8c2c518f7408b4e114e455
Aleksbgbg <kozichka01@abv.bg>
2019-01-05 17:06:20 +0000

    [UsersController] Add UsersController with Index view.

---
+++ Identity Platform/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿namespace Identity.Platform.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = "User, Admin")]
+    public class UsersController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
