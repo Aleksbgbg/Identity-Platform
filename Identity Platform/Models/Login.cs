efa745e96729f91fb757809bdc9b6ed1e4131d9c
Aleksbgbg <kozichka01@abv.bg>
2019-01-05 16:45:26 +0000

    [Login] Add simple model containing a Username and Password.

---
+++ Identity Platform/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿namespace Identity.Platform.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Login
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [UIHint("Password")]
+        public string Password { get; set; }
+    }
+}
