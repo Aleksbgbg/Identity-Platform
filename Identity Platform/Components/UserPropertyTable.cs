f2d269e16c3eb65d36d0274c35b134a8d4d50ce1
Aleksbgbg <kozichka01@abv.bg>
2019-01-05 22:16:12 +0000

    [UserPropertyTable] Add component which renders a property table of the current IdentityUser's properties.

---
+++ Identity Platform/Components/UserPropertyTable.cs
@@ -0,0 +1,24 @@
+﻿namespace Identity.Platform.Components
+{
+    using System.Threading.Tasks;
+
+    using Identity.Platform.Models;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserPropertyTable : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserPropertyTable(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _userManager.GetUserAsync(HttpContext.User));
+        }
+    }
+}
