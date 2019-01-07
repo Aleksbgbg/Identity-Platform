af27e1982ed3138f3492b3d96af683ad0e34ffb3
Aleksbgbg <kozichka01@abv.bg>
2019-01-07 11:09:47 +0000

    [SignUpCredentials] Add model which represents a username, password, and repeated password, used for signing up.

---
+++ Identity Platform/Models/SignUpCredentials.cs
@@ -0,0 +1,20 @@
+﻿namespace Identity.Platform.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SignUpCredentials
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [UIHint("Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Repeat Password")]
+        [UIHint("Password")]
+        public string RepeatPassword { get; set; }
+    }
+}
