85eb1333c51421e99f04d79eb2c422e8269fbe0a
Aleksbgbg <kozichka01@abv.bg>
2019-01-07 12:33:53 +0000

    [EnsureReturnUrlExistsFilter] Add filter which populates the 'ReturnUrl' argument of handlers if it does not exist. Useful for POST handlers which may have their data tampered with.

---
+++ Identity Platform/Attributes/Filters/EnsureReturnUrlExistsFilter.cs
@@ -0,0 +1,31 @@
+﻿namespace Identity.Platform.Attributes.Filters
+{
+    using System.Collections.Generic;
+
+    using Identity.Platform.Controllers;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.AspNetCore.Mvc.Routing;
+
+    public class EnsureReturnUrlExistsFilter : ActionFilterAttribute
+    {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+
+        public EnsureReturnUrlExistsFilter(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            IDictionary<string, object> actionArguments = context.ActionArguments;
+
+            if (!actionArguments.ContainsKey("ReturnUrl") || string.IsNullOrWhiteSpace((string)actionArguments["ReturnUrl"]))
+            {
+                actionArguments["ReturnUrl"] = _urlHelperFactory.GetUrlHelper(context)
+                                                                .Action(nameof(HomeController.Index), "Home");
+            }
+        }
+    }
+}
