3efbf4653bdd2958f02733fb1590b3e07be15d5e
Aleksbgbg <kozichka01@abv.bg>
2019-01-07 13:42:46 +0000

    [UserInfo] Add view model which represents basic info about a user, including their roles.

---
+++ Identity Platform/Models/ViewModels/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace Identity.Platform.Models.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class UserInfo
+    {
+        public UserInfo(string id, string userName, string email, IEnumerable<string> roles)
+        {
+            Id = id;
+            UserName = userName;
+            Email = email;
+            Roles = roles;
+        }
+
+        public string Id { get; }
+
+        public string UserName { get; }
+
+        public string Email { get; }
+
+        public IEnumerable<string> Roles { get; }
+    }
+}
