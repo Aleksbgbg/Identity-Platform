d83329376d2a2f2a4baf371712a823341005d8d3
Aleksbgbg <kozichka01@abv.bg>
2019-01-07 11:02:46 +0000

    [ModelStateDictionaryExtensions] Add extensions which populate the ModelStateDictionary with IdentityError(s).

---
+++ Identity Platform/Extensions/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Identity.Platform.Extensions
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+    internal static class ModelStateDictionaryExtensions
+    {
+        internal static void AddIdentityErrors(this ModelStateDictionary modelState, IdentityResult identityResult)
+        {
+            modelState.AddIdentityErrors(identityResult.Errors);
+        }
+
+        internal static void AddIdentityErrors(this ModelStateDictionary modelState, IEnumerable<IdentityError> errors)
+        {
+            foreach (IdentityError error in errors)
+            {
+                modelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+    }
+}
