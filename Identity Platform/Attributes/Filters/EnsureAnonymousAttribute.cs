00063820d8775389884b4e5767fe6fa514e8fde4
Aleksbgbg <kozichka01@abv.bg>
2019-01-08 11:13:43 +0000

    [EnsureAnonymousAttribute] Add filter which redirects requests if the user is signed in.

---
+++ Identity Platform/Attributes/Filters/EnsureAnonymousAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Identity.Platform.Attributes.Filters
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class EnsureAnonymousAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new RedirectResult("/");
+            }
+        }
+    }
+}
