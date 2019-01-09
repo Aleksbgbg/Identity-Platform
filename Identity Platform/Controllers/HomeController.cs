1c02bdcebdb85a130687c23e4eb93800bce2e1d2
Aleksbgbg <kozichka01@abv.bg>
2019-01-09 19:40:02 +0000

    [HomeController] Remove UserID route parameter (thus removing the need for a model).

--- Identity Platform/Controllers/HomeController.cs
+++ Identity Platform/Controllers/HomeController.cs
@@ -1,18 +1,11 @@
 ﻿namespace Identity.Platform.Controllers
 {
-    using System.Security.Claims;
-
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                return View((object)User.FindFirst(ClaimTypes.NameIdentifier).Value);
-            }
-
             return View();
         }
     }
