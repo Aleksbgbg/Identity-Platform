fda40830fde8da50219f3c7fc9978f58e05b8a2a
Aleksbgbg <kozichka01@abv.bg>
2019-01-18 20:38:39 +0000

    [Migrations] Add user image extension migrations.

---
+++ Identity Platform/Migrations/20190118200657_UserImages.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.Platform.Migrations
+{
+    public partial class UserImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageExtension",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageExtension",
+                table: "AspNetUsers");
+        }
+    }
+}
