df8fbeeb1d5619660ff8f5e542043a591ab0cfd9
Aleksbgbg <kozichka01@abv.bg>
2019-01-18 21:17:40 +0000

    [UserLogin] Add view model with a user and a boolean indicating whether they are the authenticated user.

---
+++ Identity Platform/Models/ViewModels/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿namespace Identity.Platform.Models.ViewModels
+{
+    public class UserLogin
+    {
+        public UserLogin(AppUser user, bool isLoggedIn)
+        {
+            User = user;
+            IsLoggedIn = isLoggedIn;
+        }
+
+        public AppUser User { get; }
+
+        public bool IsLoggedIn { get; }
+    }
+}
