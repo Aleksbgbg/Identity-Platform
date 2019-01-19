bb1d828627576531c6389ad5c572179247036e3e
Aleksbgbg <kozichka01@abv.bg>
2019-01-19 14:05:39 +0000

    [update-image] Add script to watch user image edit and display an image preview.

---
+++ Identity Platform/wwwroot/js/update-image.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function() {
+    const image = $("img#user-image");
+
+    $("input.form-control-file").change(function() {
+        if (this.files === undefined || this.files[0] === undefined) {
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = function(e) {
+            image.attr("src", e.target.result);
+        }
+
+        fileReader.readAsDataURL(this.files[0]);
+    });
+});
