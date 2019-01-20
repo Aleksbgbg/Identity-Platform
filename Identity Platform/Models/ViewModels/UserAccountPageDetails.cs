3ffe82a80e7d7bc44ba564b1a1137044bb4109ff
Aleksbgbg <kozichka01@abv.bg>
2019-01-20 12:11:18 +0000

    [UserAccountPageDetails] rename UserLogin to UserAccountPageDetails, and add a collection of comments.

---
+++ Identity Platform/Models/ViewModels/UserAccountPageDetails.cs
@@ -0,0 +1,23 @@
+﻿namespace Identity.Platform.Models.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class UserAccountPageDetails
+    {
+        public UserAccountPageDetails(AppUser user, bool isAuthenticatedUser, IEnumerable<string> roles, IEnumerable<Comment> comments)
+        {
+            User = user;
+            IsAuthenticatedUser = isAuthenticatedUser;
+            Roles = roles;
+            Comments = comments;
+        }
+
+        public AppUser User { get; }
+
+        public bool IsAuthenticatedUser { get; }
+
+        public IEnumerable<string> Roles { get; }
+
+        public IEnumerable<Comment> Comments { get; }
+    }
+}
