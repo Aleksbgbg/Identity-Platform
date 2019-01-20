7495b2ce68dbc6be9bea21a90cc65095bfa96f45
Aleksbgbg <kozichka01@abv.bg>
2019-01-20 21:52:08 +0000

    [PagingInfo] Add class to store information about pagination.

---
+++ Identity Platform/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace Identity.Platform.Models.ViewModels
+{
+    using System;
+
+    public class PagingInfo
+    {
+        public PagingInfo(int currentPage, int totalItems, int itemsPerPage)
+        {
+            CurrentPage = currentPage;
+            ItemsPerPage = itemsPerPage;
+            TotalPages = (int)Math.Ceiling((double)totalItems / itemsPerPage);
+        }
+
+        public int CurrentPage { get; }
+
+        public int ItemsPerPage { get; }
+
+        public int TotalPages { get; }
+    }
+}
