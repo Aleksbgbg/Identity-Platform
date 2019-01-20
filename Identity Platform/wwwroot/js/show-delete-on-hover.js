7c46693945868e6463e6301be10fb1f9d6cd4cd4
Aleksbgbg <kozichka01@abv.bg>
2019-01-20 21:06:10 +0000

    [show-delete-on-hover] Add script to hide and show delete comment button based on hover.

---
+++ Identity Platform/wwwroot/js/show-delete-on-hover.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    $("div.comment").hover(
+        function(event) {
+            $(event.currentTarget)
+                .find("button.btn-delete-comment")
+                .css("visibility", "visible");
+        },
+        function(event) {
+            $(event.currentTarget)
+                .find("button.btn-delete-comment")
+                .css("visibility", "hidden");
+        }
+    );
+});
