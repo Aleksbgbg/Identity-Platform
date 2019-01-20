738a5d8f230186860857202b72297f43da39dfae
Aleksbgbg <kozichka01@abv.bg>
2019-01-20 16:14:52 +0000

    [UserAccountPageDetails] Remove IsAuthenticatedUser property, and make Comments IQueryable.

--- Identity Platform/Models/ViewModels/UserAccountPageDetails.cs
+++ Identity Platform/Models/ViewModels/UserAccountPageDetails.cs
@@ -1,23 +1,21 @@
 ﻿namespace Identity.Platform.Models.ViewModels
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public class UserAccountPageDetails
     {
-        public UserAccountPageDetails(AppUser user, bool isAuthenticatedUser, IEnumerable<string> roles, IEnumerable<Comment> comments)
+        public UserAccountPageDetails(AppUser user, IEnumerable<string> roles, IQueryable<Comment> comments)
         {
             User = user;
-            IsAuthenticatedUser = isAuthenticatedUser;
             Roles = roles;
             Comments = comments;
         }
 
         public AppUser User { get; }
 
-        public bool IsAuthenticatedUser { get; }
-
         public IEnumerable<string> Roles { get; }
 
-        public IEnumerable<Comment> Comments { get; }
+        public IQueryable<Comment> Comments { get; }
     }
 }
