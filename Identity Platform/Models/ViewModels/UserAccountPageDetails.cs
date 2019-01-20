75f870862c328a5014adccdaffcd06442e566509
Aleksbgbg <kozichka01@abv.bg>
2019-01-20 21:52:34 +0000

    [UserAccountPageDetails] Add PagingInfo for comments.

--- Identity Platform/Models/ViewModels/UserAccountPageDetails.cs
+++ Identity Platform/Models/ViewModels/UserAccountPageDetails.cs
@@ -5,11 +5,14 @@
 
     public class UserAccountPageDetails
     {
-        public UserAccountPageDetails(AppUser user, IEnumerable<string> roles, IQueryable<Comment> comments)
+        private const int CommentsPerPage = 5;
+
+        public UserAccountPageDetails(AppUser user, IEnumerable<string> roles, IQueryable<Comment> comments, int currentCommentsPage)
         {
             User = user;
             Roles = roles;
             Comments = comments;
+            CommentsPagingInfo = new PagingInfo(currentCommentsPage, comments.Count(comment => comment.OwnerId == user.Id), CommentsPerPage);
         }
 
         public AppUser User { get; }
@@ -17,5 +20,7 @@
         public IEnumerable<string> Roles { get; }
 
         public IQueryable<Comment> Comments { get; }
+
+        public PagingInfo CommentsPagingInfo { get; }
     }
 }
