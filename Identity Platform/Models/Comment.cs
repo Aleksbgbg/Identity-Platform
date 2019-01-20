857c4a457db41be0f0991c3f600b892bcc6bbfa5
Aleksbgbg <kozichka01@abv.bg>
2019-01-20 12:05:33 +0000

    [Comment] Add model representing a comment on the user's account page.

---
+++ Identity Platform/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Identity.Platform.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Comment
+    {
+        // User whose wall the comment appears on
+        public string OwnerId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
