4f1a67e4498692c76b946ab2cd2dc98ad49f9e68
Aleksbgbg <kozichka01@abv.bg>
2019-01-23 08:10:30 +0000

    [RouteParameterConstraint] Add constraint that ensures an action method receives a certain route parameter.

---
+++ Identity Platform/Attributes/Constraints/RouteParameterConstraintAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Identity.Platform.Attributes.Constraints
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc.Abstractions;
+    using Microsoft.AspNetCore.Mvc.ActionConstraints;
+    using Microsoft.AspNetCore.Routing;
+
+    public class RouteParameterConstraintAttribute : ActionMethodSelectorAttribute
+    {
+        private readonly string[] _routeParameters;
+
+        public RouteParameterConstraintAttribute(params string[] routeParameters)
+        {
+            _routeParameters = routeParameters;
+        }
+
+        public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+        {
+            return _routeParameters.All(routeContext.RouteData.Values.ContainsKey);
+        }
+    }
+}
